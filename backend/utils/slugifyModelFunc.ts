6c0533fea5adfb16e01fb0f5d0ed77aed16d5637
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2023-07-10 13:28:04 +0300

    feat: func for slugify | category&menu models

---
+++ backend/utils/slugifyModelFunc.ts
@@ -0,0 +1,20 @@
+import { Schema } from 'mongoose';
+import slugify from 'slugify';
+
+const slugifyModelFunc = (schema: Schema, name: string) => {
+  schema.index({ slug: 1 });
+
+  schema.pre('save', function (next: any) {
+    this.slug = slugify(this[name], { lower: true });
+    next();
+  });
+
+  schema.pre('findOneAndUpdate', function (next: any) {
+    const update = this.getUpdate() as any;
+    if (!update || !update.name) return next();
+    update.slug = slugify(update.name, { lower: true });
+    next();
+  });
+};
+
+export default slugifyModelFunc;
