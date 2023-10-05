fca0a2f719bfa9313b950491477cbc187599f53a
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2023-10-05 14:31:36 +0300

    configure dotenv-vault

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,32 +1,32 @@
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
 import { useGetUser } from '../../hooks/use-user';
 import UserImage from '../UI/UserImage';
 
-type Inputs = {
-  name: string;
-  image?: File;
-};
+// type Inputs = {
+//   name: string;
+//   image?: File;
+// };
 
 const Profile = () => {
   const userQuery = useGetUser();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<Inputs>({
-    mode: 'onTouched',
-    defaultValues: {
-      name: userQuery.data?.name,
-    },
-  });
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors, isValid },
+  // } = useForm<Inputs>({
+  //   mode: 'onTouched',
+  //   defaultValues: {
+  //     name: userQuery.data?.name,
+  //   },
+  // });
 
-  const registers = {
-    name: register('name', {
-      required: true,
-    }),
-    image: register('image'),
-  };
+  // const registers = {
+  //   name: register('name', {
+  //     required: true,
+  //   }),
+  //   image: register('image'),
+  // };
 
   return (
     <main>
